4a621603a5466a7fab4b29577e48dae2835a13ce
IasminSaba <49463066+IasminSaba@users.noreply.github.com>
2019-06-05 23:11:05 -0300

    Update Questao10.java

--- Lista2/src/Questoes/Questao10.java
+++ Lista2/src/Questoes/Questao10.java
@@ -5,13 +5,15 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Random;
 
+//Os métodos da questão 10 estão em SeparateChainingHashMap
+
 public class Questao10 {
 
 	
 	public static void main(String[] args) {
 		Random gerador = new Random();
 		gerador.nextInt();
-		System.out.println(gerador.nextInt(30)); //s� somar 1 para iniciar de 1
+		System.out.println(gerador.nextInt(30)); //só somar 1 para iniciar de 1
 		SeparateChainingHashMap sh = new SeparateChainingHashMap(10);
 		
 		for(int i =0;i<1000;i++) {
@@ -27,13 +29,13 @@ public class Questao10 {
 	
 }
 /*
-Podemos fazer esse m�todo sem o resize e que apresenta os seguintes resultados:
+Podemos fazer esse método sem o resize e que apresenta os seguintes resultados:
 Para 1000: A maior lista possui tamanho 121 e a menor 0;
 Para 10000: A maior lista possui tamanho 126 e a menor 0;
 Para 100000: A maior lista possui tamanho 130 e a menor 0;
 Para 1000000: A maior lista possui tamanho 136 e a menor 0;
 
 Percebe-se que a maior lista difere pouca coisa de um caso para outro. Isso se deve ao aumento no tamanho do array.
-Para utilizar o risize do put teria que ser colocado um tamanho fixo que deveria abranger at� 1000000 e as listas teriam tamanhos menores.
+Para utilizar o risize do put teria que ser colocado um tamanho fixo que deveria abranger até 1000000 e as listas teriam tamanhos menores.
 
 */
